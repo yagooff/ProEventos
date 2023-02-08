50dcf9db3960affbba692fdbf180f5f761b13dd3
Yago Dias <yago.dias9@gmail.com>
2023-02-08 15:01:02 -0300

    Angular, Intermpolação, Diretiva e Binding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -10,19 +10,49 @@ import { Component } from '@angular/core';
 })
 export class EventosComponent {
 
-public eventos: any;
+public eventos: any = [];
+public eventosFiltrados: any =[]
+larguraImagem: number = 150;
+margemImagem: number = 2;
+exibirImagem: boolean = true;
+private _filtroLista: string = "";
+
+public get filtroLista(): string {
+  return this._filtroLista;
+}
+
+public set filtroLista(value: string) {
+  this._filtroLista = value;
+  this.eventosFiltrados = this.filtroLista
+    ? this.filtrarEventos(this.filtroLista)
+    : this.getEventos();
+}
+
+filtrarEventos(filtrarPor: string): any{
+  filtrarPor = filtrarPor.toLowerCase();
+  return this.eventos.filter(
+    (evento: any) => evento.tema.toLowerCase().indexOf(filtrarPor) !== -1 ||
+    evento.local.toLowerCase().indexOf(filtrarPor) !== -1
+  )
+}
 
   constructor(private http: HttpClient){}
 
     ngOnInit(): void{
       this.getEventos();
     }
+    alterarImagem(){
+      this.exibirImagem = !this.exibirImagem;
+    }
 
       public getEventos(): void{
 
         this.http.get('http://localhost:5000/api/eventos').subscribe({
-          next: res => this.eventos = res,
-          error: err => console.error(err)
+          next: res => {
+            this.eventos = res
+            this.eventosFiltrados = this.eventos;
+          },
+          error: err => { console.error(err)}
       });
     }
   }
